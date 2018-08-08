14ed21a245eb1b4e5e0fdf93f3ab3f2b1eb42c15
e5ash <e5ash.bro@gmail.com>
2018-08-09 01:07:32 +0300

    html

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,18 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+	function pixelPerfect(name, opacity, width) {
+		var ppBlock = $('.'+name),
+				ppImage = ppBlock.attr('data-pp');
+		console.log(ppBlock);
+		ppBlock.css({
+			'opacity': '0.' + opacity,
+			'position': 'absolute',
+			'transform': 'translateX(-50%)',
+			'left': '50%',
+			'z-index': '100',
+			'width': width + 'px' 
+		});
+	}
+	// pixelPerfect('pp', 3, 1920);
 });
